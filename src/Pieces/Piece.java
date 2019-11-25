4a8938a5a1f2a128dbab609a334de3e95d534a45
Halmai Erik <erikhalmai@hotmail.com>
2019-11-25 12:29:56 +0200

    Creating Piece.java

--- src/Pieces/Piece.java
+++ src/Pieces/Piece.java
@@ -2,9 +2,13 @@ package Tiles;
 
 import java.awt.Color;
 
+/**
+ * Each piece is made of tiles in a specific formation
+ */
+
 public enum TileTypes {
     /**
-     * Piece TypeI.
+     * Piece I
      */
     TypeI(new Color(BoardPanel.COLOR_MIN, BoardPanel.COLOR_MAX, BoardPanel.COLOR_MAX), 4, 4, 1, new boolean[][] {
             {
@@ -34,7 +38,7 @@ public enum TileTypes {
     }),
 
     /**
-     * Piece TypeJ.
+     * Piece J
      */
     TypeJ(new Color(BoardPanel.COLOR_MIN, BoardPanel.COLOR_MIN, BoardPanel.COLOR_MAX), 3, 3, 2, new boolean[][] {
             {
@@ -60,7 +64,7 @@ public enum TileTypes {
     }),
 
     /**
-     * Piece TypeL.
+     * Piece L
      */
     TypeL(new Color(BoardPanel.COLOR_MAX, 127, BoardPanel.COLOR_MIN), 3, 3, 2, new boolean[][] {
             {
@@ -86,7 +90,7 @@ public enum TileTypes {
     }),
 
     /**
-     * Piece TypeO.
+     * Piece O
      */
     TypeO(new Color(BoardPanel.COLOR_MAX, BoardPanel.COLOR_MAX, BoardPanel.COLOR_MIN), 2, 2, 2, new boolean[][] {
             {
@@ -108,7 +112,7 @@ public enum TileTypes {
     }),
 
     /**
-     * Piece TypeS.
+     * Piece S
      */
     TypeS(new Color(BoardPanel.COLOR_MIN, BoardPanel.COLOR_MAX, BoardPanel.COLOR_MIN), 3, 3, 2, new boolean[][] {
             {
@@ -134,7 +138,7 @@ public enum TileTypes {
     }),
 
     /**
-     * Piece TypeT.
+     * Piece T
      */
     TypeT(new Color(128, BoardPanel.COLOR_MIN, 128), 3, 3, 2, new boolean[][] {
             {
@@ -160,7 +164,7 @@ public enum TileTypes {
     }),
 
     /**
-     * Piece TypeZ.
+     * Piece Z
      */
     TypeZ(new Color(BoardPanel.COLOR_MAX, BoardPanel.COLOR_MIN, BoardPanel.COLOR_MIN), 3, 3, 2, new boolean[][] {
             {
@@ -184,4 +188,70 @@ public enum TileTypes {
                     true,	false,	false,
             }
     });
+
+    private Color colorPiece;
+    private int spawnRow;
+    private int spawnCol;
+
+    //dimension of piece's matrix: a matrix of size dimensionXdimension
+    private int dimension;
+    //nr of rows that have true in tiles matrix (when rotation = 0)
+    private int row;
+    //nr of coloumns that have true in tiles matrix (when rotation = 0)
+    private int col;
+
+    //tile matrix for this piece
+    private boolean[][] tiles;
+
+    /**
+     * Creates a new piece
+     * @param color
+     * @param dimension
+     * @param cols
+     * @param rows
+     * @param tiles
+     */
+    private TileTypes(Color color, int dimension, int cols, int rows, boolean[][] tiles) {
+        this.colorPiece = color;
+        this.dimension = dimension;
+        this.tiles = tiles;
+        this.col = col;
+        this.row = row;
+
+        this.spawnCol = 5 - (dimension >> 1);
+        this.spawnRow =
+    }
+
+    /**
+     * Gets color of piece.
+     * @return Color of piece
+     */
+    public Color getColorPiece() {
+        return colorPiece;
+    }
+
+    /**
+     * Gets dimension of piece
+     * @return Dimension of piece's matrix
+     */
+    public int getDimension() {
+        return dimension
+    }
+
+
+    public int getSpawnRow() {
+        return spawnRow;
+    }
+
+    public int getSpawnCol() {
+        return spawnCol;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public int getCol() {
+        return col;
+    }
 }
